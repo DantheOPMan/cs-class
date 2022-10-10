ba80b4f6fe1bcbcb1fec4657dacb219638b5ee39
DantheOPMan <soccerdan34@gmail.com>
2022-10-10 18:22:17 -0400

    yay

---
+++ 280/Lexical Analyzer/lex.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <iomanip>
+#include <algorithm>
+#include <ctype.h>
+#include <cctype>
+#include <cstring>
+#include <array>
+#include <map>
+
+using namespace std;
+
+LexItem getNextToken (istream& in, int& linenumber){
+   
+}
