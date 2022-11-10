1c722f75116a7c8c7f7e56b8606c164b5cd4122e
DantheOPMan <soccerdan34@gmail.com>
2022-11-10 17:44:48 -0500

    lab 7

--- 280/Lab 7/balancedDelimiters.cpp
+++ 280/Lab 7/balancedDelimiters.cpp
@@ -7,12 +7,50 @@
 using namespace std;
 
 bool BalancedDelimiters(string & inputString){
-    stack<char> myStack;
-    char inputChar[inputString.length() + 1] ;
-    strcpy(inputChar, inputString.c_str());
+    vector<string> myvector;
     for(int i = 0; i < inputString.length(); i++){
-        if(inputChar[i] == '('){
-
+        if(inputString.substr(i,1) == "(" || inputString.substr(i,1) == "{" || inputString.substr(i,1) == "["){
+            myvector.push_back(inputString.substr(i,1));
+        }
+        if(inputString.substr(i,1) == ")" && myvector.size() > 0){
+            if(myvector[myvector.size()-1] != "("){
+                if(i + 1 == inputString.length()){
+                    cout << "Stack is not empty and the scanning of input string is completed." << endl;
+                }else{
+                    cout << "Stack is not empty and the scanning of input string is not completed." << endl;
+                }
+                return false;
+            }
+            myvector.pop_back();
+        }else if(inputString.substr(i,1) == "}" && myvector.size() > 0){
+            if(myvector[myvector.size()-1] != "{"){
+                if(i + 1 == inputString.length()){
+                    cout << "Stack is not empty and the scanning of input string is completed." << endl;
+                }else{
+                    cout << "Stack is not empty and the scanning of input string is not completed." << endl;
+                }               
+                return false;
+            }
+            myvector.pop_back();
+        }else if(inputString.substr(i,1) == "]" && myvector.size() > 0){
+            if(myvector[myvector.size()-1] != "["){
+                if(i + 1 == inputString.length()){
+                    cout << "Stack is not empty and the scanning of input string is completed." << endl;
+                }else{
+                    cout << "Stack is not empty and the scanning of input string is not completed." << endl;
+                }
+                return false;
+            }
+            myvector.pop_back();
+        }else if(myvector.size() == 0 && (inputString.substr(i,1) == "]" || inputString.substr(i,1) == "}" || inputString.substr(i,1) == ")")){
+            cout << "Stack is empty and the scanning of input string is not completed." << endl;
+            return false;
         }
     }
+    if(myvector.size() >= 1){
+        cout << "Stack is not empty and the scanning of input string is completed." << endl;
+        return false;
+    }
+    cout << "Stack is empty and the scanning of input string is completed." << endl;
+    return true;
 }
