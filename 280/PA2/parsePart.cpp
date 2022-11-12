57e4b1dc33eeedfb6341bdb62ff1d35828c65011
DantheOPMan <soccerdan34@gmail.com>
2022-11-12 02:38:55 -0500

    asd

---
+++ 280/PA2/parsePart.cpp
@@ -0,0 +1,132 @@
+/* Implementation of Recursive-Descent Parser
+ * parse.cpp
+ * Programming Assignment 2
+ * Fall 2022
+*/
+
+#include "parse.h"
+
+map<string, bool> defVar;
+
+namespace Parser {
+	bool pushed_back = false;
+	LexItem	pushed_token;
+
+	static LexItem GetNextToken(istream& in, int& line) {
+		if( pushed_back ) {
+			pushed_back = false;
+			return pushed_token;
+		}
+		return getNextToken(in, line);
+	}
+
+	static void PushBackToken(LexItem & t) {
+		if( pushed_back ) {
+			abort();
+		}
+		pushed_back = true;
+		pushed_token = t;	
+	}
+
+}
+
+static int error_count = 0;
+
+int ErrCount()
+{
+    return error_count;
+}
+
+void ParseError(int line, string msg)
+{
+	++error_count;
+	cout << line << ": " << msg << endl;
+}
+
+bool IdentList(istream& in, int& line);
+
+
+//ControlStmt ::= AssigStmt | IfStmt | PrintStmt 
+bool ControlStmt(istream& in, int& line) {
+	bool status;
+	
+	LexItem t = Parser::GetNextToken(in, line);
+	
+	switch( t.GetToken() ) {
+
+	case PRINT:
+		status = PrintStmt(in, line);
+		break;
+
+	case IF:
+		status = IfStmt(in, line);
+		break;
+
+	case IDENT:
+		Parser::PushBackToken(t);
+        status = AssignStmt(in, line);
+		break;
+		
+	default:
+		Parser::PushBackToken(t);
+		return false;
+	}
+
+	return status;
+}//End of ControlStmt
+
+
+//PrintStmt:= PRINT (ExpreList) 
+bool PrintStmt(istream& in, int& line) {
+	LexItem t;
+	
+	t = Parser::GetNextToken(in, line);
+	if( t != LPAREN ) {
+		
+		ParseError(line, "Missing Left Parenthesis");
+		return false;
+	}
+	
+	bool ex = ExprList(in, line);
+	
+	if( !ex ) {
+		ParseError(line, "Missing expression list after Print");
+		return false;
+	}
+	
+	t = Parser::GetNextToken(in, line);
+	if(t != RPAREN ) {
+		
+		ParseError(line, "Missing Right Parenthesis");
+		return false;
+	}
+	return ex;
+}//End of PrintStmt
+
+//ExprList:= Expr {,Expr}
+bool ExprList(istream& in, int& line) {
+	bool status = false;
+	status = Expr(in, line);
+	if(!status){
+		ParseError(line, "Missing Expression");
+		return false;
+	}
+	
+	LexItem tok = Parser::GetNextToken(in, line);
+	
+	if (tok == COMMA) {
+		status = ExprList(in, line);
+	}
+	else if(tok.GetToken() == ERR){
+		ParseError(line, "Unrecognized Input Pattern");
+		cout << "(" << tok.GetLexeme() << ")" << endl;
+		return false;
+	}
+	else{
+		Parser::PushBackToken(tok);
+		return true;
+	}
+	return status;
+}//End of ExprList
+
+
