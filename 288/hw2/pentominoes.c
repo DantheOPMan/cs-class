4d60337086fa7b600c12be061437920bed32ba8e
DantheOPMan <soccerdan34@gmail.com>
2023-02-21 05:47:18 -0500

    finished 288

--- 288/hw2/pentominoes.c
+++ 288/hw2/pentominoes.c
@@ -3,35 +3,36 @@
 #include <string.h>
 
 void saveShape(int shapeReturn[4][4], int shapeInput[4][4]);
-void placeShapeOntoField(int field[10][12], int shape[4][4]);
-void clearLine(int field[10][12]);
+void placeShapeOntoField(int field[12][10], int shape[4][4], int position);
+int checkSpace(int field[12][10], int shape[4][4], int currentI, int currentJ);
+void clearLine(int field[12][10]);
 void rotateShape(int arr[4][4]);
 void shiftspaces(int arr[4][4]);
-void outputField(int field[10][12]);
+void outputField(int field[12][10]);
 
 int main(int argc, char *argv[]) {
     char c;
-    int field[10][12];
+    int field[12][10];
     int i, j;
-    int b[4][4] = {{0,0,1,0}, {0,1,1,1}, {0,1,0,0}, {0,0,0,0}};
-    int d[4][4] = {{0,0,0,1}, {1,1,1,1}, {0,0,0,0}, {0,0,0,0}};
-    int g[4][4] = {{0,1,1,1}, {0,1,1,0}, {0,0,0,0}, {0,0,0,0}};
-    int h[4][4] = {{0,0,1,1}, {1,1,1,0}, {0,0,0,0}, {0,0,0,0}};
-    int m[4][4] = {{0,0,0,1}, {0,0,1,1}, {0,1,1,0}, {0,0,0,0}};
-    int n[4][4] = {{0,0,1,0}, {0,1,1,1}, {0,0,1,0}, {0,0,0,0}};
+    int b[4][4] = {{0,0,0,0}, {0,1,1,0}, {1,1,0,0}, {0,1,0,0}};
+    int d[4][4] = {{0,1,0,0}, {0,1,0,0}, {0,1,0,0}, {1,1,0,0}};
+    int g[4][4] = {{0,0,0,0}, {1,1,0,0}, {1,1,0,0}, {1,0,0,0}};
+    int h[4][4] = {{0,1,0,0}, {0,1,0,0}, {1,1,0,0}, {1,0,0,0}};
+    int m[4][4] = {{0,0,0,0}, {0,0,1,0}, {0,1,1,0}, {1,1,0,0}};
+    int n[4][4] = {{0,0,0,0}, {0,1,0,0}, {1,1,1,0}, {0,1,0,0}};
     char rotationText[100] = "";
     int rotations;
     char position;
     int shape[4][4];
+    int caseNum;
     FILE *file;
 
     if (argc != 2) {
         printf("Usage: %s filename\n", argv[0]);
         return 1;
     }
-
+    
     file = fopen(argv[1], "r");
-
     if (file == NULL) {
         printf("Unable to open file: %s\n", argv[1]);
         return 1;
@@ -39,58 +40,77 @@ int main(int argc, char *argv[]) {
 
     /*File is now open and ready to read or write to.*/
     
-    for(i=0; i< 10; i++){
-        for(j=0; i< 12; j++){
+    for(i=0; i < 12; i++){
+        for(j=0; j < 10; j++){
             field[i][j] = 0;
         }  
+        
     }
-    
-
+    caseNum = 0;
     while ((c = fgetc(file)) != EOF) {
         
-        /*skip if space*/
-        if(c == ' ' || c == '\n'){
-            continue;
-        }
-
-        if(c == 'B' || c == 'D' || c == 'G' || c == 'H' || c == 'M' || c == 'N'){
-            if(c == 'B'){
-                saveShape(shape,b);
+        switch (caseNum) {
+            case 0:
+                if(c == ' ' || c == '\n'){
+                    break;
+                }
+                if(c == 'B' || c == 'D' || c == 'G' || c == 'H' || c == 'M' || c == 'N'){
+                    ungetc(c,file);
+                    caseNum = 1;
+                }
+                if(c == 'r'){
+                    caseNum = 2;
+                }
+                if(c == 'c'){
+                    ungetc(c,file);
+                    caseNum = 3;
+                }
                 
-            }else if(c == 'D'){
-                saveShape(shape,d);
-            }else if(c == 'G'){
-                saveShape(shape,g);
-            }else if(c == 'H'){
-                saveShape(shape,h);
-            }else if(c == 'M'){
-                saveShape(shape,m);
-            }else if(c == 'N'){
-                saveShape(shape,n);
-            }
-
-            while ((c = fgetc(file)) == ' '){
-
-            }
+                continue;
+                break;
+            case 1:
+                if(c == 'B'){
+                    saveShape(shape,b);
+                }else if(c == 'D'){
+                    saveShape(shape,d);
+                }else if(c == 'G'){
+                    saveShape(shape,g);
+                }else if(c == 'H'){
+                    saveShape(shape,h);
+                }else if(c == 'M'){
+                    saveShape(shape,m);
+                }else if(c == 'N'){
+                    saveShape(shape,n);
+                }
+                c = fgetc(file);
+                caseNum = 0;
+                break;
+            case 2:
+                if(c != ' '){
+                    strncat(rotationText, &c, 1);
+                    break;
+                }
+                rotations = (atoi(rotationText)) % 4;
+                for(i =0; i < rotations; i++){
+                    rotateShape(shape);
+                }
+                shiftspaces(shape);
+                
+                caseNum = 0;
+                rotationText[0] = '\0';
+                continue;
+                break;
+            case 3:
+                position = (int)(c=fgetc(file)) - (int)'0';
+                placeShapeOntoField(field, shape, position);
+                clearLine(field);
+                caseNum = 0;
+                break;
             
-            while((c = fgetc(file)) != ' '){
-                strncat(rotationText, &c, 1);
-            }
-            rotations = (atoi(rotationText)) % 4;
-            for(i =0; i < rotations; i++){
-                rotateShape(shape);
-            }
-            shiftspaces(shape);
-
-            while ((c = fgetc(file)) == ' '){
-
-            }
-            position = (int)(fgetc(file));
-
-
         }
+        
     }
-
+    outputField(field);
     fclose(file);
     return 0;
 }
@@ -99,58 +119,72 @@ int main(int argc, char *argv[]) {
 void saveShape(int shapeReturn[4][4], int shapeInput[4][4]){
     int i, j;
     for (i = 0; i < 4; i++) {
-        for (j = i; j < 4; j++) {
+        for (j = 0; j < 4; j++) {
             shapeReturn[i][j] = shapeInput[i][j];
         }
     }
 }
 
 /*Moves shape down as much as possible*/
-void placeShapeOntoField(int field[10][12], int shape[4][4], int position){
-    int i,j;
-    for (i=0; i<11; i++){
-        for(j = position; j < position + 4; j++){
-            if(!checkSpace(field, shape, i, position){
-                
+void placeShapeOntoField(int field[12][10], int shape[4][4], int position){
+    int i, k,l;    
+    int check;
+    for (i=0; i<12; i++){
+        check = checkSpace(field, shape, i, position);
+        if(check != 0){
+            for(k=0; k < 4; k++){
+                for(l = 0; l < 4; l++){
+                    if(shape[k][l] == 1){
+                        if(check-(4-k) < 0){
+                            exit(1);
+                        }
+                        field[check+k][position + l] = shape[k][l];
+                    }
+                }
             }
-            if(i==10 || )){
-
-            }
+            break;
         }
     }
 }
 
-int checkSpace(int field[10][12], int shape[4][4], int currentI, int currentJ){
+/*Checks if the object is allowed in that spot*/
+int checkSpace(int field[12][10], int shape[4][4], int currentI, int currentJ){
     int i,j;
-
-    for (i=0; i<4; i++){
-        for(j = 0; j < 4; j++){
-            if(field[currentI+i][currentJ+j] && shape[i][j]){
-                return 0;
+    for (i=0; i< 4; i++){
+        for(j=0; j<4; j++){
+            if(field[currentI+i][currentJ+j] == 1 && shape[i][j] == 1){
+                return currentI -1;
             }
         }
     }
-    return 1;
+    if(currentI + 3 == 11){
+        return currentI;
+    }
+    
+    return 0;
 }
 
 
 /*Delete line if full*/
-void clearLine(int field[10][12]){
+void clearLine(int field[12][10]){
     int i,j,k,l;
-    for(j = 11; j > 0; j--) {
-        for(i = 0; i < 10; i++) {
+    for(i = 11; i >= 0; i--) {
+        for(j = 0; j < 10; j++) {
             if(field[i][j] == 0){
                 break;
             }
-            if(field[i][j] == 1 && i == 9){
-                for (k = j; k > 0; k--) {
+            if(j == 9){
+                for (k = i; k >= 0; k--) {
                     for (l = 0; l < 10; l++) {
-                        field[k][l] = field[k-1][l];
+                        if(k==0){
+                            field[k][l] = 0;
+                        }else{
+                            field[k][l] = field[k-1][l];
+                        }   
                     }
                 }
-                j++;
             }
-        }         
+        }  
     }
 }
 
@@ -178,9 +212,9 @@ void rotateShape(int arr[4][4]){
 /*Shift shape so that it's left and down most*/
 void shiftspaces(int arr[4][4]) {
     int i, j;
-    int is_left_column_empty;
-    int is_bottom_row_empty;
-    do{
+    int is_left_column_empty = 1;
+    int is_bottom_row_empty = 1;
+    while(is_left_column_empty || is_bottom_row_empty){
         is_left_column_empty = 1;
         is_bottom_row_empty = 1;
         for (i = 0; i < 4; i++) {
@@ -214,17 +248,17 @@ void shiftspaces(int arr[4][4]) {
                 arr[0][j] = 0;
             }
         }
-    }while(!is_left_column_empty || !is_bottom_row_empty);
+    }
     
     
 }
 
 /*Output field */
-void outputField(int field[10][12]){
+void outputField(int field[12][10]){
     int i, j;
-    for(i = 0; i < 10; i++) {
+    for(i = 0; i < 12; i++) {
         printf("|");
-        for(j = 0; j < 12; j++) {
+        for(j = 0; j < 10; j++) {
             if(field[i][j]){
                 printf("#");
             }else{
@@ -234,5 +268,5 @@ void outputField(int field[10][12]){
         }
         printf("|\n");
     }
-    printf("+-----------+\n");
+    printf("+----------+\n");
 }
